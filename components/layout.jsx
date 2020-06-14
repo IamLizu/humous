a5c9e2fd5b007514d54558b3963595c64bc3c7f0
IamLizu <thegeek@iamlizu.com>
2020-06-15 03:11:59 +0600

    Linking the pages in nextjs way

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,29 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { siteTitle } from '../data/about'
+import Link from 'next/link'
 import { Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem, NavLink } from 'reactstrap'
 import styles from './layout.module.css'
+import { annotate } from 'rough-notation';
+
 
 export default function Layout({ children, home }) {
     const [collapsed, setCollapsed] = useState(true)
 
     const toggleNavMenu = () => setCollapsed(!collapsed)
 
+    useEffect(() => {
+        const e = document.getElementById('appHeader');
+        const annotation = annotate(e, { type: 'highlight', color: '#fff176' });
+        annotation.show();
+    })
+
     return(
         <>
-
             <header className={styles.container}>
                 {home? (
                     <>
                         <Navbar color="faded" light>
-                            <NavbarBrand className="mr-auto">{siteTitle}</NavbarBrand>
+                            <NavbarBrand id="appHeader" className={`mr-auto ${styles.frontPageHeader}`}>
+                                {siteTitle}
+                            </NavbarBrand>
                             <NavbarToggler onClick={toggleNavMenu} className="mr-2"/>
                             <Collapse isOpen={!collapsed} navbar>
                                 <Nav navbar>
                                     <NavItem>
-                                        <NavLink href="/about/">About</NavLink>
+                                        <Link href="/about/" passHref>
+                                            <NavLink>About</NavLink>
+                                        </Link>
                                     </NavItem>
                                     <NavItem>
-                                        <NavLink href="/submit-post/">Submit Post</NavLink>
+                                        <Link href="/submit-post/" passHref>
+                                            <NavLink>Submit Post</NavLink>
+                                        </Link>
                                     </NavItem>
                                 </Nav>
                             </Collapse>
@@ -32,15 +46,23 @@ export default function Layout({ children, home }) {
                 ) : (
                     <>
                         <Navbar color="faded" light>
-                            <NavbarBrand href="/" className="mr-auto">{siteTitle}</NavbarBrand>
+                            <Link href="/" passHref>
+                                <NavbarBrand id="appHeader" className="mr-auto">
+                                    {siteTitle}
+                                </NavbarBrand>
+                            </Link>
                             <NavbarToggler onClick={toggleNavMenu} className="mr-2"/>
                             <Collapse isOpen={!collapsed} navbar>
                                 <Nav navbar>
                                     <NavItem>
-                                        <NavLink href="/about/">About</NavLink>
+                                        <Link href="/about/" passHref>
+                                            <NavLink>About</NavLink>
+                                        </Link>
                                     </NavItem>
                                     <NavItem>
-                                        <NavLink href="/submit-post/">Submit Post</NavLink>
+                                        <Link href="/submit-post/" passHref>
+                                            <NavLink>Submit Post</NavLink>
+                                        </Link>
                                     </NavItem>
                                 </Nav>
                             </Collapse>
